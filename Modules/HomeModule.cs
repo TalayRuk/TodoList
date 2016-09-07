b4c29eb3b592aac6f32de73d2954d8e971b3a478
Talayruk <notejde@gmail.com>
2016-09-07 13:15:26 -0700

    add save in task and post in homeModule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,9 +10,22 @@ namespace ToDoList
       Get["/"]=_=> View["add_new_task.cshtml"];
       Get["/view_all_tasks"]=_=> {
         Task newTask = new Task(Request.Query["new-task"]);
-        newTask.Save();
         return View["view_all_tasks.cshtml", newTask];
+
+      Post["/task_added"] = _ => {
+        Task newTask = new Task (Request.Form["new-task"]);
+        newTask.Save();
+        return View["task_added.cshtml", newTask];
+        //added a new route with Post instead of Get. We still instantiate a new Task Obj named newTask
+        //w/Post request, the description is not coming fr the pages'query parameters,
+        //it's coming from the form. So we use Request.Form["new-task"] instead of Request.Query
+        //Then we call save() method on newTask
+        //The request will return the View task_added.cshtml. THis is just going to be a page that display
+        // the Task we just added, newTask, so all we need to do is pass in newTask.
+        //in order for form to follow this route upon being submitted, we need to change the form in add_new_task.cshtml
+        //by changing form action to task_added and add method ="post"
       };
+
     }
   }
 }
